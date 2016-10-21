402b92c4f8bb170e3724de28b012c16fdccaeba8
Hendrikto <hendrik.to@gmail.com>
2016-10-21 23:11:22 +0200

    Convert i3status extensions to C.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: i3status_extensions
 
 i3status_extensions: i3status_extensions/i3status_extensions.c
-	g++ -Wall -O3 i3status_extensions/i3status_extensions.c -o i3status_extensions/i3status_extensions
+	gcc -Wall -O3 i3status_extensions/i3status_extensions.c -o i3status_extensions/i3status_extensions
 
 clean:
 	rm i3status_extensions/i3status_extensions
--- i3status_extensions/i3status_extensions.c
+++ i3status_extensions/i3status_extensions.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <iostream>
-#include <fstream>
+#include <stdbool.h>
 
 /**
  * A tool to prepend the output of i3status with additional information.
@@ -12,26 +11,24 @@
  */
 
 int get_max_brightness() {
-	std::ifstream max_brightness;
-	max_brightness.open("/sys/class/backlight/intel_backlight/max_brightness");
 	int max_brightness_value;
-	max_brightness >> max_brightness_value;
+	FILE* max_brightness = fopen("/sys/class/backlight/intel_backlight/max_brightness", "r");
+	fscanf(max_brightness, "%d", &max_brightness_value);
 	return max_brightness_value;
 }
 
 int main() {
 	int brightness = 0;
 	int max_brightness = get_max_brightness();
-	char* input = (char*) malloc(1);
+	char* input = malloc(1);
 	size_t size = 0;
-	std::ifstream actual_brightness;
-	actual_brightness.open("/sys/class/backlight/intel_backlight/actual_brightness");
+	FILE* actual_brightness = fopen("/sys/class/backlight/intel_backlight/actual_brightness", "r");
 	while (true) {
-		actual_brightness >> brightness;
-		actual_brightness.seekg(0);
+		fscanf(actual_brightness, "%d", &brightness);
+		rewind(actual_brightness);
 		getline(&input, &size, stdin);
 		printf("💡 %.0f%% | %s", brightness * 100 / (float) max_brightness, input);
-		std::cout.flush();
+		fflush(stdout);
 	}
 	return EXIT_SUCCESS;
 }
