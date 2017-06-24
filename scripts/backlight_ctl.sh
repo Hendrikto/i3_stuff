8c9f99a6df7cdbc2c9a51cbe4c55ed99652157fb
Hendrikto <hendrik.to@gmail.com>
2017-06-24 16:02:31 +0200

    Replace xbacklight with custom script.
    * xbacklight does not work with proprietary Nvidia drivers.

--- scripts/backlight_ctl.sh
+++ scripts/backlight_ctl.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+backlight_dir=/sys/class/backlight/intel_backlight/
+max_brightness=$(cat $backlight_dir"max_brightness")
+brightness_step=$(bc <<< $max_brightness" / 20")
 
 if [ $1 = "inc" ]; then
-	xbacklight -inc 5
+	new_brightness=$(bc -l <<< "$(cat $backlight_dir"brightness") + $brightness_step")
+	echo $new_brightness > /sys/class/backlight/intel_backlight/brightness
 elif [ $1 = "dec" ]; then
-	xbacklight -dec 5
+	new_brightness=$(bc -l <<< "$(cat $backlight_dir"brightness") - $brightness_step")
+	echo $new_brightness > /sys/class/backlight/intel_backlight/brightness
 fi
 
 killall -USR1 i3status
