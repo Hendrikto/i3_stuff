9c72a8799c2bbfba962522c2ec9e07a2bbb0f1b6
Hendrikto <hendrik.to@gmail.com>
2017-06-30 01:09:31 +0200

    Use inotify to avoid reading the brightness file unnecessarily.

--- i3status_extensions/i3status_extensions.c
+++ i3status_extensions/i3status_extensions.c
@@ -1,7 +1,11 @@
+#include <limits.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/inotify.h>
 
 #define BACKLIGHT_DIR "/sys/class/backlight/intel_backlight/"
+#define EVENT_BUFFER_SIZE sizeof(struct inotify_event) + NAME_MAX + 1
 
 /**
  * A tool to prepend the output of i3status with additional information.
@@ -24,12 +28,19 @@ int main() {
 	int brightness = 0;
 	char* input = NULL;
 	size_t size = 0;
+	int inotify_fd = inotify_init1(IN_NONBLOCK);
+	inotify_add_watch(inotify_fd, BACKLIGHT_DIR "actual_brightness", IN_MODIFY);
+	char event_buffer[EVENT_BUFFER_SIZE];
 	FILE* actual_brightness = fopen(BACKLIGHT_DIR "actual_brightness", "r");
 	setbuf(actual_brightness, NULL);
+	fscanf(actual_brightness, "%d", &brightness);
+	rewind(actual_brightness);
 	for (;;) {
 		getline(&input, &size, stdin);
-		fscanf(actual_brightness, "%d", &brightness);
-		rewind(actual_brightness);
+		if (read(inotify_fd, event_buffer, EVENT_BUFFER_SIZE) > 0) {
+			fscanf(actual_brightness, "%d", &brightness);
+			rewind(actual_brightness);
+		}
 		printf("💡 %d%% | %s", brightness * 100 / max_brightness, input);
 		fflush(stdout);
 	}
