0541dfbcc5c4b0c42c66f0b6a7cb776bde65aafc
Hendrikto <hendrik.to@gmail.com>
2019-08-04 02:40:20 +0200

    Keep it simple

--- i3status_extensions/i3status_extensions.c
+++ i3status_extensions/i3status_extensions.c
@@ -1,10 +1,5 @@
-#include <limits.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/inotify.h>
-
-#define EVENT_BUFFER_SIZE sizeof(struct inotify_event) + NAME_MAX + 1
 
 /**
  * A tool to prepend the output of i3status with additional information.
@@ -18,19 +13,14 @@ int main() {
 	int brightness = 0;
 	char* input = NULL;
 	size_t size = 0;
-	int inotify_fd = inotify_init1(IN_NONBLOCK);
-	inotify_add_watch(inotify_fd, BACKLIGHT_DIR "actual_brightness", IN_MODIFY);
-	char event_buffer[EVENT_BUFFER_SIZE];
 	FILE* actual_brightness = fopen(BACKLIGHT_DIR "actual_brightness", "r");
 	setbuf(actual_brightness, NULL);
 	fscanf(actual_brightness, "%d", &brightness);
 	rewind(actual_brightness);
 	for (;;) {
 		getline(&input, &size, stdin);
-		if (read(inotify_fd, event_buffer, EVENT_BUFFER_SIZE) > 0) {
-			fscanf(actual_brightness, "%d", &brightness);
-			rewind(actual_brightness);
-		}
+		fscanf(actual_brightness, "%d", &brightness);
+		rewind(actual_brightness);
 		printf("💡 %d%% | %s", brightness * 100 / MAX_BRIGHTNESS, input);
 		fflush(stdout);
 	}
